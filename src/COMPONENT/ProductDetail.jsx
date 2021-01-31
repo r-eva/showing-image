a64832350e063ba44f2ddc82e390436af092d8ba
regina <reginaevadewi@gmail.com>
2021-01-31 02:03:22 +0100

    detail data

--- src/COMPONENT/ProductDetail.jsx
+++ src/COMPONENT/ProductDetail.jsx
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
-import {Col, Card, Spinner, Row} from 'react-bootstrap'
+import {Col, Card, Spinner, Image} from 'react-bootstrap'
 import {urlApi} from '../HELPERS/database'
 import {Link} from 'react-router-dom'
 import LocalData from '../HELPERS/data.json'
+import './ProductDetail.css'
 
 function ProductDetail () {
     const [data, setData] = useState('')
@@ -22,19 +23,42 @@ function ProductDetail () {
     const renderProductDetail = () => {
         var jsx = data.images.map((val, idx) => {
             return (
-                <Col key={idx}>
-                    <Link to={
-                        {
-                            pathname: "/img-detail",
-                            state: {
-                                uri: val.uri,
-                                title: data.title
-                            }
+                <Link key={idx} to={
+                    {
+                        pathname: "/img-detail",
+                        state: {
+                            uri: val.uri,
+                            title: data.title
                         }
-                    }>
-                        <Card.Img src={`https://${val.uri}_2.jpg`}/>
-                    </Link>
-                </Col>
+                    }
+                } className="img-tablet_wrapper">
+                    <Image src={`https://${val.uri}_2.jpg`} className={`img-thumbnail`}/>
+                </Link>
+            )
+        })
+        return jsx
+    }
+
+    const renderClassification = () => {
+        var jsx = data.attributes.map((val, idx) => {
+            return (
+                    <div className="row" key={idx}>
+                        <div className="col-6 font-weight-bold"><p>{val.label}</p></div>
+                        <div className="col-6">
+                            <p>{val.value}</p>
+                        </div>
+                    </div>
+            )
+        })
+        return jsx
+    }
+
+    const renderFeatures = () => {
+        var jsx = data.features.map((val, idx) => {
+            return(
+                <div className="col-6" key={idx}>
+                    <p><span className="font-weight-bold">&#x2713;</span> {val}</p>
+                </div>
             )
         })
         return jsx
@@ -44,36 +68,55 @@ function ProductDetail () {
         return <Spinner animation="border" />
     }
 
+
     return (       
         <div className="m-md-5">
-
-            {/* MOBILE DEVICE */}
-
-            <div className="d-sm-none">
-                <Card>
-                    
-                    {renderProductDetail()}
-                    <Card.Body>
-                        <Card.Title className="font-weight-bold">
-                            <p>{data.title}<br/>{data.price.gross}</p>
-                        </Card.Title>
-                        <Row>
-                            
-                        </Row>
-                    </Card.Body>
-                </Card> 
+            <div className="row">
+                <div className="col-12 col-sm-7">
+                    <Card className="card-img">
+                        <Card.Body>
+                            <div className="img-tablet_container">
+                                {renderProductDetail()}
+                            </div>
+                        </Card.Body>
+                        <Card.Footer className="font-weight-bold">
+                            {data.title}<br/>{data.price.gross}
+                        </Card.Footer>
+                    </Card>
+                </div>
+                <div className="col-12 col-sm-5 mt-3 mt-md-0">
+                    <Card className="card-technische">
+                        <Card.Header className="font-weight-bold">Technische Daten</Card.Header>
+                        <Card.Body>
+                            <div className="row">
+                                <div className="col-6 font-weight-bold"><p>Preis</p></div>
+                                <div className="col-6">
+                                    <p>{data.price.grs.amount.toString()} {data.price.grs.currency} (Bruto)<br/>
+                                    {data.price.nt.amount.toString()} {data.price.nt.currency} (Netto)<br/>
+                                    {data.vat}</p>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-6 font-weight-bold"><p>Kategorie</p></div>
+                                <div className="col-6">
+                                    <p>{data.category}</p>
+                                </div>
+                            </div>
+                            {renderClassification()}
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-12 my-3">
+                    <Card>
+                        <Card.Header className="font-weight-bold">Ausstattung</Card.Header>
+                        <Card.Body>
+                            <div className="row">
+                                {renderFeatures()}
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </div>
             </div>
-
-            {/* TABLET AND DESKTOP */}
-            <div className="d-none d-md-block">
-                <Card>
-                    <Row>
-                        {renderProductDetail()}
-                    </Row>
-                </Card>
-            </div>
-
-
         </div>
        
     );
